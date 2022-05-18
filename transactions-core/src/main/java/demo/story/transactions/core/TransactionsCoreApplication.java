f6637c2daa7b1fcfd7cb3acbbdfc593844465aa7
Erick Barrera <ebarreral.isc@gmail.com>
2022-05-18 15:52:41 -0500

    starting transactions-core

---
+++ transactions-core/src/main/java/demo/story/transactions/core/TransactionsCoreApplication.java
@@ -0,0 +1,13 @@
+package demo.story.transactions.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionsCoreApplication.class, args);
+    }
+
+}
---
+++ transactions-core/src/test/java/demo/story/transactions/core/TransactionsCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.story.transactions.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
